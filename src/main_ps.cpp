9b6e9801c75fa59326edc891c5cbf2da4d46e73c
vanluwin <richardsonsantiago@ufrn.edu.br>
2021-04-11 18:24:43 -0300

    Planetary System Entry Point

---
+++ src/main_ps.cpp
@@ -0,0 +1,44 @@
+#include "../include/PlanetarySystem.h"
+
+#define FPS 30
+#define REDISPLAY_INTERVAL 1000 / FPS
+
+PlanetarySystem *planetary_system = new PlanetarySystem;
+
+void reshape(int w, int h) {
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+void timer(int) {
+    glutPostRedisplay();
+    glutTimerFunc(REDISPLAY_INTERVAL, timer, 0);
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowPosition(10, 10);
+    glutCreateWindow("Planetary System");
+
+    glutTimerFunc(REDISPLAY_INTERVAL, timer, 0);
+
+    glutDisplayFunc([]() { planetary_system->Run(); });
+    glutReshapeFunc(reshape);
+
+    glutSpecialFunc([](int key, int x, int y) { planetary_system->KeyboardHandler(key); });
+    glutKeyboardFunc([](unsigned char key, int x, int y) { planetary_system->KeyboardHandler(key); });
+
+    glutMainLoop();
+
+    return 0;
+}
